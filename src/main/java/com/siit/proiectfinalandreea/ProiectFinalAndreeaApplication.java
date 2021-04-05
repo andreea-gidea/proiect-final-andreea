cde6dfe2614e2a4bd933af6e3b421de71137a21c
Andreea <gideaandreea@yahoo.com>
2021-04-05 23:08:27 +0300

    Initial commit

---
+++ src/main/java/com/siit/proiectfinalandreea/ProiectFinalAndreeaApplication.java
@@ -0,0 +1,13 @@
+package com.siit.proiectfinalandreea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectFinalAndreeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectFinalAndreeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siit/proiectfinalandreea/ProiectFinalAndreeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siit.proiectfinalandreea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectFinalAndreeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
